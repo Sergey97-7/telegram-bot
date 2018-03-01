71b0df303fcecc4eeec174cc9581790912dfb34b
John Doe <johndoe@example.com>
2018-03-01 18:50:40 +0000

    meteor-kitchen 201803011850404023

--- client/views/bot/details/details_controller.js
+++ client/views/bot/details/details_controller.js
@@ -16,10 +16,10 @@ this.BotDetailsController = RouteController.extend({
 	},
 
 	isReady: function() {
-		var botId = this._id;
+		var id = this._id;
 
 		var subs = [
-			Meteor.subscribe("message_details", botId)
+			Meteor.subscribe("message_details", id)
 		];
 		var ready = true;
 		_.each(subs, function(sub) {
@@ -30,11 +30,11 @@ this.BotDetailsController = RouteController.extend({
 	},
 
 	data: function() {
-		var botId = this._id;
+		var id = this._id;
 
 		var data = {
 			params: this.params || {},
-			message_details: Question.findOne({_id:botId}, {})
+			message_details: Question.findOne({_id:id}, {})
 		};
 		
 
--- server/publish/question.js
+++ server/publish/question.js
@@ -6,7 +6,7 @@ Meteor.publish("messages_empty", function() {
 	return Question.find({_id:null}, {});
 });
 
-Meteor.publish("message_details", function(botId) {
-	return Question.find({_id:botId}, {});
+Meteor.publish("message_details", function(id) {
+	return Question.find({_id:id}, {});
 });
 

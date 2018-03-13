6d7f3f650621f114738932d5cec69c1431be99b9
John Doe <johndoe@example.com>
2018-03-13 17:56:47 +0000

    meteor-kitchen 201803131756475272

--- both/joins/joins.js
+++ both/joins/joins.js
@@ -1,3 +1,3 @@
 // Question
-Question.join(Question, "next_question", "question", []);
+Question.join(Question, "next_question", "question", ["Id", "bot_msg", "answer_type", "answer_var", "question", "first_question"]);
 

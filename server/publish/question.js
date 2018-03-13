6a50067f1cf3f8edcab17f75d6fcc16db4021bb7
John Doe <johndoe@example.com>
2018-03-13 21:56:25 +0000

    meteor-kitchen 201803132156254596

--- server/publish/question.js
+++ server/publish/question.js
@@ -1,12 +1,12 @@
 Meteor.publish("messages", function() {
-	return Question.publishJoinedCursors(Question.find({}, {sort:[["Id","asc"]]}));
+	return Question.find({}, {sort:[["Id","asc"]]});
 });
 
 Meteor.publish("messages_empty", function() {
-	return Question.publishJoinedCursors(Question.find({_id:null}, {}));
+	return Question.find({_id:null}, {});
 });
 
 Meteor.publish("message_details", function(botId) {
-	return Question.publishJoinedCursors(Question.find({_id:botId}, {}));
+	return Question.find({_id:botId}, {});
 });
 

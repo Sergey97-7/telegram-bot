457a78027e02f49a266c77ad4a618ca16560a45a
John Doe <johndoe@example.com>
2018-03-15 11:31:57 +0000

    meteor-kitchen 201803151131575229

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import bot from './telebot.js'
 var verifyEmail = false;
 
 Accounts.config({ sendVerificationEmail: verifyEmail });
 
 Meteor.startup(function() {
+	bot.fillDb();
+	const app = new bot();
 	// read environment variables from Meteor.settings
 	if(Meteor.settings && Meteor.settings.env && _.isObject(Meteor.settings.env)) {
 		for(var variableName in Meteor.settings.env) {
--- server/telebot.js
+++ server/telebot.js
@@ -1,11 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import TelegramBot from 'telegram-bot-api'
+//const Messages = new Mongo.Collection('messages');
 
 export default class bot
 {
     constructor()
     {
+
+
+//const que = Messages.find({text: 'Hello, world!' });
+
+
 
         //Creates telegram bot object
         this.bot = new TelegramBot
@@ -17,14 +23,23 @@ export default class bot
             }
 
         });
+
         const app = this;
         //Binding telegram to our meteor environment and start listening incoming messages
 		this.bot.on('message', Meteor.bindEnvironment(function(message)
 			{
 				app.receiveMessage(message.from.id, message.text, message.from.first_name)
 				//Here we can receive messages
-			}));
+			}
+            ));
     }
+    static fillDb()
+    {
+      //  Messages.insert({ text: 'Hello, world!' }, {name:'jane'});
+       // Messages.insert({ text: 1 });
+        
+    }
+
 //sends message to user using telegram bot API
    sendMessage(userId, text)
     {
@@ -43,7 +58,19 @@ export default class bot
             }
         });
     }
-  //  var question = question.find({},{fields: {Id: 1}}).fetch();
+
+    //const que = Messages.findone({}).fetch();
+  // var question = question.find({},{fields: {Id:2}}).fetch();
+ // const que = Question.find({Id:1}).fetch();
+ //const ques = Messages.find();
+
+ //Question.findOne({Id:1});
+// que(userId,text)
+ //   {
+  //      let textToSend; 
+ //       get(textToSend);
+ //       this.sendMessage(userId, textToSend);
+ //   }
     Err(userId,text)
     {
     	let textToSend = 'Некорректные данные! \nВведите \ "/start" \ для просмотра доступных функций';
@@ -64,9 +91,12 @@ export default class bot
    			case '/start':
      			this.Bot_start(from);
     		break;
-    		case '/question':
-     			this.sendMessage(userId,question);
-     		break;  		
+    		//case '/question':
+     		//	this.sendMessage(userId,que,username);
+     		//break; 
+           // case '/question':
+          //    this.que(from);
+          //  break; 		
        		default:
 				this.Err(from);
 			break;

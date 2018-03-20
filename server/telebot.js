3f72272b4598e7b84651571ec8b9794f66572dfc
John Doe <johndoe@example.com>
2018-03-20 18:43:53 +0000

    meteor-kitchen 201803201843530933

--- server/telebot.js
+++ server/telebot.js
@@ -190,7 +190,8 @@ export default class bot
 
     // var a2 = Answer.findOne({answer_var: a1, "answer_var": a}); // курсор на строчку в таблице ответов по ответу
      var a2 = Answer.findOne({answer_var: a1});
-   var a3 = a2.question.bot_msg;
+     var nn = Answer.findOne();
+   var a3 = nn.question.bot_msg;
     // var a3 = 
     //  var a3 = a2.question; //задается следующий вопрос
     // let textToSend = a3; 
@@ -198,10 +199,12 @@ export default class bot
      //this.sendMessage(userId, textToSend);
     // console.log(q1);
     // var nn = Question.findOne();
-     console.log(q1);
+    console.log(nn);
+    console.log(a3)
+     /*console.log(q1);
      console.log(a1);
      console.log(a2);
-     console.log(a3);
+     console.log(a3);*/
      //console.log(nn);
     // console.log(a3);
    // console.log(a2);
